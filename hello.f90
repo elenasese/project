6404e93b2ffd2f0e867da8815d64a2a937e93bb4
Elena Sese <elenasesesansa@gmail.com>
2017-02-22 14:50:29 +0100

    third version file

--- hello.f90
+++ hello.f90
@@ -1,11 +1,14 @@
 PROGRAM helloworld
 IMPLICIT NONE
-CHARACTER(len=50):: name
+CHARACTER(len=50):: name,surname
 
 print*, "enter you name"
 read*, name
 
-print*, "hello", name, "!"
+print*, "enter surname"
+read*, surname
+
+print*, "hello", name,surname, "!"
 
 write(*,*) 'FIRST DIRECTORY. HELLO WORLD'
 
